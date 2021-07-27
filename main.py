216edbce2f8db6cbe5e380197744352b25e8af82
Diego <lima.j.diego@gmail.com>
2021-07-27 13:24:22 -0300

    Using chatterbot

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
+# Nome do meu chatbot
+chatbot = ChatBot('Petrus')
+
+trainer = ListTrainer(chatbot)
+
+trainer.train([
+    'Hello!',
+    'Hi, how are you',
+    'I fine and you:'
+])
+
+while True:
+    request = input('You: ')
+    response = chatbot.get_response(request)
+    print('Bot: ', response)
+
